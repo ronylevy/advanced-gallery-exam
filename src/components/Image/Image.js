b061eb9b2f104cf8989ef6ebf51ea5ae6687d54c
Rony <levy95rony@gmail.com>
2021-01-15 13:59:11 +0200

    Rotate button is working

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -1,29 +1,43 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import FontAwesome from 'react-fontawesome';
-import './Image.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import FontAwesome from "react-fontawesome";
+import "./Image.scss";
 
 class Image extends React.Component {
   static propTypes = {
     dto: PropTypes.object,
-    galleryWidth: PropTypes.number
+    galleryWidth: PropTypes.number,
   };
 
   constructor(props) {
     super(props);
     this.calcImageSize = this.calcImageSize.bind(this);
     this.state = {
-      size: 200
+      size: 200,
+      rotation: 0,
     };
+
+    // this.rotate = this.rotate.bind(this);
   }
 
   calcImageSize() {
-    const {galleryWidth} = this.props;
+    const { galleryWidth } = this.props;
     const targetSize = 200;
     const imagesPerRow = Math.round(galleryWidth / targetSize);
-    const size = (galleryWidth / imagesPerRow);
+    const size = galleryWidth / imagesPerRow;
     this.setState({
-      size
+      size,
+    });
+  }
+
+  rotate() {
+    console.log(this);
+    let newRotation = this.state.rotation + 90;
+    if (newRotation >= 360) {
+      newRotation = -360;
+    }
+    this.setState({
+      rotation: newRotation,
     });
   }
 
@@ -36,19 +50,26 @@ class Image extends React.Component {
   }
 
   render() {
+    const { rotation } = this.state;
     return (
       <div
         className="image-root"
         style={{
+          transform: `rotate(${rotation}deg)`,
           backgroundImage: `url(${this.urlFromDto(this.props.dto)})`,
-          width: this.state.size + 'px',
-          height: this.state.size + 'px'
+          width: this.state.size + "px",
+          height: this.state.size + "px",
         }}
-        >
+      >
         <div>
-          <FontAwesome className="image-icon" name="sync-alt" title="rotate"/>
-          <FontAwesome className="image-icon" name="trash-alt" title="delete"/>
-          <FontAwesome className="image-icon" name="expand" title="expand"/>
+          <FontAwesome
+            className="image-icon"
+            name="sync-alt"
+            title="rotate"
+            onClick={this.rotate.bind(this)}
+          />
+          <FontAwesome className="image-icon" name="trash-alt" title="delete" />
+          <FontAwesome className="image-icon" name="expand" title="expand" />
         </div>
       </div>
     );
