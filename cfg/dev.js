38c0bb1a05ca2914f42c6b6f9dfc4fe236aba033
Rony <levy95rony@gmail.com>
2021-01-24 17:08:51 +0200

    proper commit

--- cfg/dev.js
+++ cfg/dev.js
@@ -9,7 +9,6 @@ let defaultSettings = require('./defaults');
 let BowerWebpackPlugin = require('bower-webpack-plugin');
 
 let config = Object.assign({}, baseConfig, {
-<<<<<<< HEAD
     entry: [
         'webpack-dev-server/client?http://127.0.0.1:' + defaultSettings.port,
         'webpack/hot/only-dev-server',
@@ -25,28 +24,10 @@ let config = Object.assign({}, baseConfig, {
         })
     ],
     module: defaultSettings.getDefaultModules()
-=======
-  entry: [
-    'webpack-dev-server/client?http://127.0.0.1:' + defaultSettings.port,
-    'webpack/hot/only-dev-server',
-    './src/index'
-  ],
-  cache: true,
-  devtool: 'eval-source-map',
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
-    new BowerWebpackPlugin({
-      searchResolveModulesDirectories: false
-    })
-  ],
-  module: defaultSettings.getDefaultModules()
->>>>>>> 54da43cda7bbcc34d8350a5d7ef65d904bc75acb
 });
 
 // Add needed loaders to the defaults here
 config.module.loaders.push({
-<<<<<<< HEAD
     test: /\.(js|jsx)$/,
     loader: 'react-hot!babel-loader',
     include: [].concat(
@@ -55,14 +36,3 @@ config.module.loaders.push({
 });
 
 module.exports = config;
-=======
-  test: /\.(js|jsx)$/,
-  loader: 'react-hot!babel-loader',
-  include: [].concat(
-    config.additionalPaths,
-    [ path.join(__dirname, '/../src') ]
-  )
-});
-
-module.exports = config;
->>>>>>> 54da43cda7bbcc34d8350a5d7ef65d904bc75acb
